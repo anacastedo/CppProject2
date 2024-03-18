b3ab2a7cbd1d9f8b57d3c8efc41d8ed5cc10258e
Ana Castedo <73062200+anacastedo@users.noreply.github.com>
2024-03-18 14:47:00 +0000

    Converter using mod operation

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ int main()
 { 
     // converter of cents to different coins
     int cents;
+    int cents2;
     
     cout << "Enter an amount in cents: "; 
     cin >> cents ;
+    cents2 = cents;
 
     int dollars = cents / 100;
     cents -= dollars*100;
@@ -20,6 +22,24 @@ int main()
     int nickels = cents / 5;
     cents -= nickels*5;
     int pennies = cents;
+    
+    cout << "\nYou can provide this change as follow:\n" << "dollars: " << dollars << endl;
+    cout << "quarters: " << quarters << endl;
+    cout << "dimes: " << dimes << endl;
+    cout << "nickels:  " << nickels << endl;
+    cout << "pennies: " << pennies << endl;
+    
+    //using mod
+    
+    dollars = cents2 / 100;
+    cents2 = cents2 % 100;
+    quarters = cents2 / 25;
+    cents2 = cents2 % 25;
+    dimes = cents2 / 10;
+    cents2 = cents2 % 10;
+    nickels = cents2 / 5;
+    cents2 = cents2 % 5;
+    pennies = cents2;
     
     cout << "\nYou can provide this change as follow:\n" << "dollars: " << dollars << endl;
     cout << "quarters: " << quarters << endl;
